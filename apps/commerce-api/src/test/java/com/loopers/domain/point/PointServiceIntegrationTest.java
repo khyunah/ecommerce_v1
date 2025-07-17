62be3581f7e6e432318ca700f912faaa7a25d74b
Hyunah Kim <yd3085@naver.com>
2025-07-18 02:12:56 +0900

    test: Point 조회 통합테스트
    회원ID 존재시 보유포인트 반환, 존재하지 않을시 null 반환

---
+++ apps/commerce-api/src/test/java/com/loopers/domain/point/PointServiceIntegrationTest.java
@@ -0,0 +1,55 @@
+package com.loopers.domain.point;
+
+import com.loopers.support.error.CoreException;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional
+public class PointServiceIntegrationTest {
+    private PointService pointSpyService;
+    @Autowired
+    private PointRepository pointRepository;
+
+    @BeforeEach
+    void setUp() {
+        PointService realService = new PointService(pointRepository);
+        pointSpyService = Mockito.spy(realService);
+    }
+
+    @DisplayName("해당 ID 의 회원이 존재할 경우, 보유 포인트가 반환된다.")
+    @Test
+    void returnUserInfo_whenIdExists() {
+        // given
+        Point point = new Point("test124", 10000);
+        Point save = pointRepository.save(point);
+
+        // when
+        Point selectPoint = pointSpyService.get(point.getUserId());
+        int savedPoint = save.getPoint();
+
+        // then
+        assertThat(savedPoint).isEqualTo(selectPoint.getPoint());
+    }
+
+    @DisplayName("해당 ID 의 회원이 존재하지 않을 경우, null 이 반환된다.")
+    @Test
+    void returnNull_whenUserIdNotFound() {
+        // given
+        String userId = "test1212";
+        // when
+        Point point = pointSpyService.get(userId);
+
+        // then
+        assertThat(point).isNull();
+    }
+
+}
