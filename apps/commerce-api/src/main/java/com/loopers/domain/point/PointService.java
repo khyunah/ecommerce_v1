5d679c5aa8c6b5f7673339ad5e1b5cd157ab5c6f
Hyunah Kim <yd3085@naver.com>
2025-07-18 02:34:11 +0900

    feat: PointService 충전 기능 추가

--- apps/commerce-api/src/main/java/com/loopers/domain/point/PointService.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/point/PointService.java
@@ -1,5 +1,7 @@
 package com.loopers.domain.point;
 
+import com.loopers.support.error.CoreException;
+import com.loopers.support.error.ErrorType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -17,4 +19,13 @@ public class PointService {
         return point;
     }
 
+    public Point charge(Point point, int amount){
+        Point getPoint = pointRepository.findByUserId(point.getUserId()).orElse(null);
+        if(null == getPoint){
+            throw new CoreException(ErrorType.NOT_FOUND, "존재하지 않는 유저 ID 로 충전을 시도했습니다.");
+        }
+
+        Point charged = Point.charge(getPoint, amount);
+        return pointRepository.save(charged);
+    }
 }
