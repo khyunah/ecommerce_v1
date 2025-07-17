9d5dec100b8aa817251f8e4a066b114c40d8dd81
Hyunah Kim <yd3085@naver.com>
2025-07-18 02:11:54 +0900

    feat: PointRepository 인터페이스
    Repository 인터페이스 생성 및 JPA, RepositoryImpl 구현

---
+++ apps/commerce-api/src/main/java/com/loopers/domain/point/PointRepository.java
@@ -0,0 +1,8 @@
+package com.loopers.domain.point;
+
+import java.util.Optional;
+
+public interface PointRepository {
+    Optional<Point> findByUserId(String userId);
+    Point save(Point point);
+}
