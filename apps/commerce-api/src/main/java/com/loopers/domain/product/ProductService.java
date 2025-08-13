1b8fe700b08ef98ffb13009f4e288c309c8c22f1
Hyunah Kim <yd3085@naver.com>
2025-08-14 03:00:18 +0900

    feat: 상품 상세 조회 캐시 적용

--- apps/commerce-api/src/main/java/com/loopers/domain/product/ProductService.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/product/ProductService.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Component;
 public class ProductService {
     public final ProductRepository productRepository;
 
+    @Cacheable(cacheNames = "product", key = "#id")
     public Product getDetail(Long id) {
         return productRepository.findById(id)
                 .orElseThrow(() -> new CoreException(ErrorType.NOT_FOUND, "상품 ID가 존재하지 않습니다."));
